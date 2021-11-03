18edd9a44fff40e6ab4fd7171008192e72ff9111
Nelson <1456@edmonds.edu>
2021-11-03 16:54:28 -0700

    Part 4: How to Create a REST API with Express.js in Node.js

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -7,7 +7,19 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.send("Received a GET request");
+});
+
+app.post('/', (req, res) => {
+    res.send("Received a POST request");
+});
+
+app.put('/', (req, res) => {
+    res.send("Received a PUT request");
+});
+
+app.delete('/', (req, res) => {
+    res.send("Received a DELETE request");
 });
 
 app.listen(process.env.PORT, () => 
