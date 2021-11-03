387d8e417bdb36a2fc75097ccd3979f6c81e2b1a
Nelson <1456@edmonds.edu>
2021-11-03 15:15:13 -0700

    Part 3: using process.env.PORT

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -11,5 +11,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(process.env.PORT, () => 
-    console.log('Example app listening on port 3000!'),
+    console.log(`Example app listening on port ${process.env.PORT}!`),
 );
