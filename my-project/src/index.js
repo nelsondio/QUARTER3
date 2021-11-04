2d60a665bbcf6ff17ce1f1ad8633e9ce40cb86f6
Nelson <1456@edmonds.edu>
2021-11-04 15:06:48 -0700

    Part 4: Modular Models in Express as Data Sources

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
+import models from './models';
 
 const app = express();
 
@@ -13,6 +14,15 @@ app.use(cors());
 // Our middleware to authenticate user
 app.use((req, res, next) =>{
     req.me = users[1];
+    next();
+});
+
+// in Modular Models in Express as Data sources. CONTEXT
+app.use( (req, res) => {
+    req.context = {
+        models,
+        me: models.users[1],
+    };
     next();
 });
 
@@ -32,12 +42,18 @@ app.delete('/', (req, res) => {
     res.send("Received a DELETE request");
 });
 
+// ROUTE SESSION as pseudo authentication
+app.get('/session', (req, res) => {
+    //return res.send(users[req.me.id]); // outside variable, not PURE
+    return res.send(req.context.models.users[req.context.me.id]);
+});
+
 app.get('/users', (req, res) => {
-    res.send(Object.values(users));
+    res.send(Object.values(req.context.models.users));
 });
 
 app.get('/users/:userId', (req, res) => {
-    res.send(users[req.params.userId]);
+    res.send(req.context.models.users[req.params.userId]);
 });
 
 app.post('/users', (req, res) => {
@@ -54,11 +70,11 @@ app.delete('/users', (req, res) => {
 
 // MESSAGES ROUTES
 app.get('/messages', (req, res) => {
-    res.send(Object.values(messages));
+    res.send(Object.values(req.context.models.messages));
 });
 
 app.get('/messages/:messageId', (req, res) => {
-    res.send(messages[req.params.messageId]);
+    res.send(req.context.models.messages[req.params.messageId]);
 });
 
 app.post('/messages', (req, res) => {
@@ -66,9 +82,9 @@ app.post('/messages', (req, res) => {
     const message = {
         id,
         text: req.body.text,
-        userId: req.me.id,
+        userId: req.context.me.id,
     };
-    messages[id] = message; // object property initialization
+    req.context.models.messages[id] = message; // object property initialization
     
     return res.send(message);
 });
@@ -81,9 +97,9 @@ app.delete('/messages/:messageId', (req, res) => {
     const {
         [req.params.messageId]: message,
         ...otherMessages
-    } = messages;
+    } = req.context.models.messages;
     // we use spread operator to filter out the message with a given ID
-    messages = otherMessages;
+    req.context.models.messages = otherMessages;
     
     return res.send(messages);
 });
@@ -92,27 +108,10 @@ app.delete('/messages/:messageId', (req, res) => {
 app.listen(process.env.PORT, () => 
     console.log(`Example app listening on port ${process.env.PORT}!`),
 );
-
-let users = {
-    1: {
-        id: '1',
-        userName: 'Robin Wieruch',
-    },
-    2: {
-        id: '2',
-        userName: 'Dave Davids',
-    },
-};
-
-let messages = {
-    1: {
-        id:'1',
-        text: 'Hello World',
-        userId: '1',
-    },
-    2: {
-        id: '2',
-        text: 'Bye World',
-        userId: '2',
-    },
-};
+/*
+We are using application-wide middleware to pass the models to all 
+routes in a context object. Models are living outside src/index and 
+can be refactored to actual db interfaces later . No global variables,
+no side-effect of No PURE functions, passing context via the request
+object with the context object. Now move routes out .
+*/
---
+++ my-project/src/models/index.js
@@ -0,0 +1,29 @@
+
+let users = {
+    1: {
+        id: '1',
+        userName: 'Robin Wieruch',
+    },
+    2: {
+        id: '2',
+        userName: 'Dave Davids',
+    },
+};
+
+let messages = {
+    1: {
+        id:'1',
+        text: 'Hello World',
+        userId: '1',
+    },
+    2: {
+        id: '2',
+        text: 'Bye World',
+        userId: '2',
+    },
+};
+
+export default {
+    users, 
+    messages,
+}
