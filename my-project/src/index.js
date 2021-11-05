7c31e8f2a420cdc3c79530f08e3d6e30090b761a
Nelson <1456@edmonds.edu>
2021-11-05 14:06:51 -0700

    Setup MongoDb with Mongoose in Epress. Added eraseDatabaseOnSync, routes changed to asyn/await syntax, local middleware context fixed, session shwoing wieruch logged in, database seed created and session key is by login not by ID

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -7,22 +7,27 @@ import routes from './routes';
 
 const app = express();
 
-// Built-in middleware
+// * Application-Level Middleware * //
+
+// Third-Party Middleware
 app.use(cors());
+
+// Built-in middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Our middleware to authenticate user DELETED
 
-// in Modular Models in Express as Data sources. CONTEXT
-app.use((req, res, next) => {
+// Custom middleware in Modular Models in Express as Data sources. CONTEXT 
+app.use(async (req, res, next) => {
     req.context = {
         models,
-        me: models.users[1],
+        me: await models.User.findByLogin('rwieruch'),
     };
     next();
 });
 
+// * ROUTES * //
 // Mount routes as modular routes. Each route receives a URI
 app.use('/session', routes.session);
 app.use('/users', routes.user);
@@ -36,10 +41,51 @@ app.use('/messages', routes.message);
 
 // MESSAGES ROUTES
 
+
+// * START * //
 // SERVER PORT LISTENING
 // Connect asynchronously and start listening
+const eraseDatabaseOnSync = true;
+
 connectDb().then(async () => {
+    if (eraseDatabaseOnSync){
+        await Promise.all([
+            models.User.deleteMany({}),
+            models.Message.deleteMany({}),
+        ]);
+
+        createUserWithMessages();
+    }
     app.listen(process.env.PORT, () =>
         console.log(`Example app listening on port ${process.env.PORT}!`),
     );
 });
+
+const createUserWithMessages = async () => {
+    const user1 = new models.User({
+        userName: 'rwieruch',
+    });
+    const user2 = new models.User({
+        userName: 'ddavis',
+    });
+    const message1 = new models.Message({
+        text: 'Published the ROAD TO LEARN REACT',
+        user: user1.id,
+    });
+    const message2 = new models.Message({
+        text: 'Happy to release ......',
+        user: user2.id,
+    });
+    const message3 = new models.Message({
+        text: 'Published a complete...... ',
+        user: user2.id,
+    });
+
+    await message1.save();
+    await message2.save();
+    await message3.save();
+
+    await user1.save();
+    await user2.save();
+    
+};
