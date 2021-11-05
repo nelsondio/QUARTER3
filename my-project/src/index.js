0a654e8baac171c97e6256901972ca2203ecbd69
Nelson <1456@edmonds.edu>
2021-11-05 12:21:28 -0700

    Mongoose added, connectDb().then async listening added, eraseDatabaseOnSync not yet added

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 
-import models from './models';
+import models, { connectDb } from './models';
 import routes from './routes';
 
 const app = express();
@@ -10,12 +10,12 @@ const app = express();
 // Built-in middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded( { extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // Our middleware to authenticate user DELETED
 
 // in Modular Models in Express as Data sources. CONTEXT
-app.use( (req, res, next) => {
+app.use((req, res, next) => {
     req.context = {
         models,
         me: models.users[1],
@@ -29,23 +29,7 @@ app.use('/users', routes.user);
 app.use('/messages', routes.message);
 
 
-
-app.get('/', (req, res) => {
-    res.send("Received a GET request");
-});
-
-app.post('/', (req, res) => {
-    res.send("Received a POST request");
-});
-
-app.put('/', (req, res) => {
-    res.send("Received a PUT request");
-});
-
-app.delete('/', (req, res) => {
-    res.send("Received a DELETE request");
-});
-
+// ROOT ROUTES
 // ROUTE SESSION as pseudo authentication
 
 // ROUTES '/users'
@@ -53,13 +37,9 @@ app.delete('/', (req, res) => {
 // MESSAGES ROUTES
 
 // SERVER PORT LISTENING
-app.listen(process.env.PORT || 8888, () => 
-    console.log(`Example app listening on port ${process.env.PORT}!`),
-);
-/*
-We are using application-wide middleware to pass the models to all 
-routes in a context object. Models are living outside src/index and 
-can be refactored to actual db interfaces later . No global variables,
-no side-effect of No PURE functions, passing context via the request
-object with the context object. Now move routes out .
-*/
+// Connect asynchronously and start listening
+connectDb().then(async () => {
+    app.listen(process.env.PORT, () =>
+        console.log(`Example app listening on port ${process.env.PORT}!`),
+    );
+});
