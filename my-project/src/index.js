aeefb6d46d034c15a9f93f334296b04244216658
Nelson <1456@edmonds.edu>
2021-11-12 09:29:21 -0800

    Pending to fix the babel staff

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello Project!.");
+import express from 'express';
+
+const app = express();
+
+app.use(3000, () => {
+    console.log("server is running on port 3000");
+})
+//console.log("Hello Project!.");
